ee3920b079e04a326ec6327bf9e25c8d770e4126
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-08-18 18:35:34 -0300

    dasdsa

--- front/src/helpers/getProductCategory.ts
+++ front/src/helpers/getProductCategory.ts
@@ -1,12 +1,10 @@
-// helpers/productsCategory.ts
 import { IProduct } from "@/interfaces/IProduct";
-import { getProducts } from "./getProducts"; // tu función que trae todos los productos
+import axiosInstance from "@/api/axiosInstance";
 
-export const getProductsByCategory = async (categoryName: string): Promise<IProduct[]> => {
+export const getProductsByCategory = async (categoryId: string): Promise<IProduct[]> => {
   try {
-    const allProducts = await getProducts(); // traemos todos los productos
-    // filtramos por categoría sin importar género
-    return allProducts.filter(product => product.category.name.toLowerCase() === categoryName.toLowerCase());
+    const res = await axiosInstance.get(`/products/category/${categoryId}`);
+    return res.data; // Axios ya parsea JSON automáticamente
   } catch (error) {
     console.error("Error fetching products by category:", error);
     return [];
