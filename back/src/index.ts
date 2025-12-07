231b92e659ab403e8eba5f4c9efdf6bebea4a93d
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-12-07 00:35:15 -0300

    das

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,11 +1,14 @@
-import "dotenv/config"; // <<--- ESTO VA PRIMERO SIEMPRE
+import "dotenv/config"; 
 import "reflect-metadata";
 import { AppDataSource } from "./config/dataSource";
 import app from "./server";
 import { PORT } from "./config/envs";
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("Entities cargadas:", AppDataSource.options.entities);
+
 const initialize = async () => {
-  console.log("DATABASE_URL:", process.env.DATABASE_URL); // DEBUG
+  console.log("DATABASE_URL:", process.env.DATABASE_URL); 
   console.log("Initializing server");
   
   try {
