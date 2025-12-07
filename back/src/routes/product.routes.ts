39a3acac6934b4a9bfb7bd9f86109f12711e4d4c
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-12-06 23:56:44 -0300

    das

--- back/src/routes/product.routes.ts
+++ back/src/routes/product.routes.ts
@@ -1,14 +1,24 @@
-
 import { Router } from 'express';
-import { getProducts, getProduct, createProduct, updateProduct, deleteProduct, getProductsByCategoryController } from '../controllers/product.controller';
+import { 
+  getProducts, 
+  getProduct, 
+  createProduct, 
+  updateProduct, 
+  deleteProduct, 
+  getProductsByCategoryController 
+} from '../controllers/product.controller';
 
 const router = Router();
 
+// Primero las rutas específicas
+router.get('/category/:id', getProductsByCategoryController);
+
+// Luego las rutas generales
 router.get('/', getProducts);
 router.get('/:id', getProduct);
+
 router.post('/', createProduct);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
-router.get('/category/:id', getProductsByCategoryController);
 
 export default router;
